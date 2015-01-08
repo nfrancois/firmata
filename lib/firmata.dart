814379741fe81ce22ebfaec42f59c03c611a2ee2
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2015-01-08 14:46:05 +0100

    fix library name

--- lib/firmata.dart
+++ lib/firmata.dart
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-library serial_port;
+library firmata;
 
 import 'dart:async';
 import 'dart:collection';
