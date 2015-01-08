7dc3039031b6641a34ae0c263831bb91f81b28dc
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2015-01-08 10:16:54 +0100

    fix library name

--- lib/src/board.dart
+++ lib/src/board.dart
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-part of serial_port;
+part of firmata;
 
 /// Pin usage modes
 class PinModes {
--- lib/src/sysex_parser.dart
+++ lib/src/sysex_parser.dart
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-part of serial_port;
+part of firmata;
 
 /// Parser which read message sent from arduino
 class SysexParser {
