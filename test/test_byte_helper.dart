99e90d48f64b6bc4794ef98756baee85ca06e61c
Nicolas François <nicolas.franc@gmail.com>
2015-04-23 14:48:05 +0000

    Migrate to test package

--- test/test_byte_helper.dart
+++ test/test_byte_helper.dart
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'package:unittest/unittest.dart';
+import 'package:test/test.dart';
 import 'package:firmata/src/firmata_internal.dart';
 import 'dart:async';
 
--- test/test_sysex_parser.dart
+++ test/test_sysex_parser.dart
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'package:unittest/unittest.dart';
+import 'package:test/test.dart';
 import 'package:firmata/src/firmata_internal.dart';
 import 'dart:async';
 
@@ -44,224 +44,207 @@ void main() {
 
     // Tested object
     SysexParser parser;
+    
+    Timer timeOut(int seconds) =>  
+      new Timer(new Duration(seconds: seconds), () {
+        fail('event not fired in time');
+      });
 
-    test('Report version', (){
-        // given
+    test('Report version only connexion', () async {
+        // Given
         parser = new SysexParser();
-
-        // then
-        parser.onFirmataVersion.first.then(expectAsync((FirmataVersion version){
-            expect(version.major, 2);
-            expect(version.minor, 3);
-            expect(version.name, 'StandardFirmata.ino');
-        }));
-
-        new Timer(new Duration(seconds: 1), () {
-          fail('event not fired in time');
-        });
-
-        // when
+        final t = timeOut(1);
+        
+        // When
         parser.append(CONNEXION_BYTES);
+        FirmataVersion version = await parser.onFirmataVersion.first;
 
+        // Then        
+        expect(version.major, 2);
+        expect(version.minor, 3);
+        expect(version.name, 'StandardFirmata.ino');
+        
+        t.cancel();
     });
 
-    test('Report version', (){
-        // given
+    test('Report version full version', () async {
+        // Given
         parser = new SysexParser();
-
-        // then
-        parser.onFirmataVersion.first.then(expectAsync((FirmataVersion version){
-            expect(version.major, 2);
-            expect(version.minor, 3);
-            expect(version.name, 'StandardFirmata.ino');
-        }));
-
-        new Timer(new Duration(seconds: 1), () {
-          fail('event not fired in time');
-        });
-
-        // when
+        final t = timeOut(1);
+        
+        // When
         parser.append(FIRMWARE_QUERY_RESPONSE_BYTES);
+        FirmataVersion version = await parser.onFirmataVersion.first;
 
+        // Then        
+        expect(version.major, 2);
+        expect(version.minor, 3);
+        expect(version.name, 'StandardFirmata.ino');
+        
+        t.cancel();
     });
 
-    test('Digital message', (){
-      // given
+    test('Digital message', () async {
+      // Given
       parser = new SysexParser(true);
-
-      // then
-      parser.onDigitalMessage.first.then(expectAsync((Map<int, int> pinState){
-        expect(pinState.length, 8);
-        expect(pinState[0], 0);
-        expect(pinState[1], 0);
-        expect(pinState[2], 1);
-        expect(pinState[3], 1);
-        expect(pinState[4], 0);
-        expect(pinState[5], 0);
-        expect(pinState[6], 0);
-        expect(pinState[7], 0);
-      }));
-
-      new Timer(new Duration(seconds: 1), () {
-        fail('event not fired in time');
-      });
-
-      // when
+      final t = timeOut(1);
+      
+      // When
       parser.append([0x90, 0x0C, 0x00]);
+      Map<int, int> pinState = await parser.onDigitalMessage.first;
+      
+      // Then
+      expect(pinState.length, 8);
+      expect(pinState[0], 0);
+      expect(pinState[1], 0);
+      expect(pinState[2], 1);
+      expect(pinState[3], 1);
+      expect(pinState[4], 0);
+      expect(pinState[5], 0);
+      expect(pinState[6], 0);
+      expect(pinState[7], 0);
+
+      t.cancel();
+      
     });
 
-    test('Digital message other pins', (){
-      // given
+    test('Digital message other pins', () async {
+      // Given
       parser = new SysexParser(true);
-
-      // then
-      parser.onDigitalMessage.first.then(expectAsync((Map<int, int> pinState){
-        expect(pinState.length, 8);
-        expect(pinState[8], 0);
-        expect(pinState[9], 0);
-        expect(pinState[10], 1);
-        expect(pinState[11], 1);
-        expect(pinState[12], 0);
-        expect(pinState[13], 0);
-        expect(pinState[14], 0);
-        expect(pinState[15], 0);
-      }));
-
-      new Timer(new Duration(seconds: 1), () {
-        fail('event not fired in time');
-      });
-
-      // when
+      final t = timeOut(1);
+      
+      // When
       parser.append([0x90, 0x0C, 0x01]);
+      Map<int, int> pinState = await parser.onDigitalMessage.first;
+      
+      // Then
+      expect(pinState.length, 8);
+      expect(pinState[8], 0);
+      expect(pinState[9], 0);
+      expect(pinState[10], 1);
+      expect(pinState[11], 1);
+      expect(pinState[12], 0);
+      expect(pinState[13], 0);
+      expect(pinState[14], 0);
+      expect(pinState[15], 0);
+
+      t.cancel();
     });
 
-    test('Analog message on pin', (){
-      // given
+    test('Analog message on pin', () async {
+      // Given
       parser = new SysexParser(true);
+      final t = timeOut(1);
 
-      // then
-      parser.onAnalogMessage.first.then(expectAsync((Map<int, int> pinState){
-        expect(pinState.length, 1);
-        expect(pinState[1], 148);
-      }));
-
-      new Timer(new Duration(seconds: 1), () {
-        fail('event not fired in time');
-      });
-
-      // when
+      // When
       parser.append([0xE1, 0x14, 0x01]);
+      Map<int, int> pinState = await parser.onAnalogMessage.first;
+      
+      // Then
+      expect(pinState.length, 1);
+      expect(pinState[1], 148);
+      
+      t.cancel();
     });
 
-    test('Analog message on last pin', (){
-      // given
+    test('Analog message on last pin', () async {
+      // Given
       parser = new SysexParser(true);
-
-      // then
-      parser.onAnalogMessage.first.then(expectAsync((Map<int, int> pinState){
-        expect(pinState.length, 1);
-        expect(pinState[15], 148);
-      }));
-
-      new Timer(new Duration(seconds: 1), () {
-        fail('event not fired in time');
-      });
-
-      // when
+      final t = timeOut(1);
+      
+      // When
       parser.append([0xEF, 0x14, 0x01]);
+      Map<int, int> pinState = await parser.onAnalogMessage.first;
+      
+      // Then
+      expect(pinState.length, 1);
+      expect(pinState[15], 148);
+
+      t.cancel();
     });
 
-    test('Analog query response', (){
-      // given
+    test('Analog query response', () async {
+      // Given
       parser = new SysexParser(true);
-
-      // then
-      parser.onAnalogMapping.first.then(expectAsync((List<int> analogPins){
-        expect(analogPins, [0, 1, 2, 3, 4, 5]);
-      }));
-
-      new Timer(new Duration(seconds: 1), () {
-        fail('event not fired in time');
-      });
-
-      //
+      final t = timeOut(1);
+      
+      // When
       parser.append(ANALOG_MAPPING_RESPONSE_BYTES);
+      List<int> analogPins = await parser.onAnalogMapping.first;
+
+      // Then
+      expect(analogPins, [0, 1, 2, 3, 4, 5]);
+
+      t.cancel();
     });
 
-    test('Capability query response', (){
-      // given
+    test('Capability query response', () async {
+      // Given
       parser = new SysexParser(true);
-
-      // then
-      parser.onCapability.first.then(expectAsync((Map<int, List<int>> capabilities){
-        expect(capabilities.length, 20);
-        expect(capabilities[0], []);
-        expect(capabilities[1], []);
-        expect(capabilities[2], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
-        expect(capabilities[3], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
-        expect(capabilities[4], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
-        expect(capabilities[5], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
-        expect(capabilities[6], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
-        expect(capabilities[7], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
-        expect(capabilities[8], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
-        expect(capabilities[9], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
-        expect(capabilities[10], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
-        expect(capabilities[11], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
-        expect(capabilities[12], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
-        expect(capabilities[13], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
-        expect(capabilities[14], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO]);
-        expect(capabilities[15], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO]);
-        expect(capabilities[16], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO]);
-        expect(capabilities[17], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO]);
-        expect(capabilities[18], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO, PinModes.I2C]);
-        expect(capabilities[19], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO, PinModes.I2C]);
-      }));
-
-      new Timer(new Duration(seconds: 1), () {
-        fail('event not fired in time');
-      });
-
-      // when
+      final t = timeOut(1);
+      
+      // When
       parser.append(CAPABILITY_QUERY_RESPONSE_BYTES);
+      Map<int, List<int>> capabilities = await parser.onCapability.first;
 
+      // Then
+      expect(capabilities.length, 20);
+      expect(capabilities[0], []);
+      expect(capabilities[1], []);
+      expect(capabilities[2], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
+      expect(capabilities[3], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
+      expect(capabilities[4], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
+      expect(capabilities[5], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
+      expect(capabilities[6], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
+      expect(capabilities[7], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
+      expect(capabilities[8], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
+      expect(capabilities[9], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
+      expect(capabilities[10], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
+      expect(capabilities[11], [PinModes.INPUT, PinModes.OUTPUT, PinModes.PWM, PinModes.SERVO]);
+      expect(capabilities[12], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
+      expect(capabilities[13], [PinModes.INPUT, PinModes.OUTPUT, PinModes.SERVO]);
+      expect(capabilities[14], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO]);
+      expect(capabilities[15], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO]);
+      expect(capabilities[16], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO]);
+      expect(capabilities[17], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO]);
+      expect(capabilities[18], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO, PinModes.I2C]);
+      expect(capabilities[19], [PinModes.INPUT, PinModes.OUTPUT, PinModes.ANALOG, PinModes.SERVO, PinModes.I2C]);
+  
+      t.cancel();
     });
 
-    test('Pin State response pin 13 is off', (){
-      // given
+    test('Pin State response pin 13 is off', () async {
+      // Given
       parser = new SysexParser(true);
-
-      // then
-      parser.onPinState.first.then(expectAsync((PinState pinState){
-        expect(pinState.pin, 13);
-        //expect(pinState.mode, PinModes.INPUT);
-        expect(pinState.value, PinValue.LOW);
-      }));
-
-      new Timer(new Duration(seconds: 1), () {
-        fail('event not fired in time');
-      });
-
-      // when
+      final t = timeOut(1);
+      
+      // When
       parser.append([0xF0, 0x6E, 0x0D, 0x01, 0x00, 0xF7]);
+      PinState pinState = await parser.onPinState.first;
+
+      // Then
+      expect(pinState.pin, 13);
+      //expect(pinState.mode, PinModes.INPUT);
+      expect(pinState.value, PinValue.LOW);
+
+      t.cancel();
     });
 
-    test('Pin State response pin 13 is on', (){
-      // given
+    test('Pin State response pin 13 is on', () async {
+      // Given
       parser = new SysexParser(true);
+      final t = timeOut(1);
 
-      // then
-      parser.onPinState.first.then(expectAsync((PinState pinState){
-        expect(pinState.pin, 13);
-        //expect(pinState.mode, PinModes.INPUT);
-        expect(pinState.value, PinValue.HIGH);
-      }));
-
-      new Timer(new Duration(seconds: 1), () {
-        fail('event not fired in time');
-      });
-
-      // when
+      // When
       parser.append([0xF0, 0x6E, 0x0D, 0x01, 0x01, 0xF7]);
+      PinState pinState = await parser.onPinState.first;
+
+      // Then
+      expect(pinState.pin, 13);
+      //expect(pinState.mode, PinModes.INPUT);
+      expect(pinState.value, PinValue.HIGH);
+
+      t.cancel();
     });
 
     // TODO analog pinState + unknow pinState
